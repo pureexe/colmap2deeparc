9e7f547303b1a2e521456cda5709cdda1cc52a0a
Pakkapon Phongthawee <me@pureapp.in.th>
2020-02-14 13:46:28 +0700

    First working ?

--- deeparch/reader.py
+++ deeparch/reader.py
@@ -3,76 +3,55 @@ from colmap.database import COLMAPDatabase
 from colmap.read_write_model import read_model
 import numpy as np
 
-def parse_intrinsic_params(model,params):
-    if model == 'SIMPLE_PINHOLE':
-        return [
-            params[1],
-            params[2],
-            1, #only 1 focal length
-            params[0],
-            0, # no distrotion
-        ]
-    elif model == 'PINHOLE':
-        return [
-            params[2],
-            params[3],
-            2, # 2 focal length
-            params[0],
-            params[1],
-            0, # no distrotion
-        ]
-    elif model == 'SIMPLE_RADIAL':
-        return [
-                    params[1],
-                    params[2],
-                    1, #only 1 focal length
-                    params[0],
-                    1, # simple_radial have 1 distrotion parameter
-                    params[3] 
-                ]
-            )
-        elif model == 'RADIAL':
-            instrinsic_info.append(
-                [
-                    params[1],
-                    params[2],
-                    1, #only 1 focal length
-                    params[0],
-                    2, # simple_radial have 1 distrotion parameter
-                    params[3],
-                    params[4]
-                ]
-            )
-        else:
-            raise RuntimeError(
-                'Camera number {} using {} which is currently not support'
-                    .format(cid, model)
-            )
 
 def binary_reader(binary_path):
     cameras, images, points3d = read_model(binary_path,'.bin')
-    point2d_info = []
-    instrinsic_info = []
-    extrinsic_info = []
-    point3d_info = []
-    point3d_lookup = {}
-    camera_lookup = {}
-
-    #build point3d
+    
+    point2d = []
+    instrinsic = []
+    extrinsic = []
+    point3d = []
+    
+    #filter only useful point3d data
     point_len = 0
     for point_id in points3d:
         pid, xyz, rgb, err, img_ids, kpt_ids = points3d[point_id]
-        point3d_lookup[pid] = point_len
-        point3d_info.append(np.concatenate((xyz, rgb)))
-        point_len = point_len + 1
+        point3d.append({
+            'id': pid,
+            'position': xyz,
+            'color': rgb
+        })
 
-    #build camera 
+    #filter only useful point3d data
     for cam_id in cameras:
         cid, model, w, h, params = cameras[cam_id]
-        #<center_x> <center_y>  <num_of_focal_length> <focal_length_0> <focal_length_1> ... <num_of_distortion> <distrotion_0> <distrotion_1>
+        instrinsic.append({
+            'id': cid, 
+            'model': model,
+            'params': params
+        })
+    
+    for image_id in images:
+        img_id, qvec, tvec, camera_id, name, xys, point3d_ids = images[image_id]
+        # filter only useful extrinsic data
+        extrinsic.append({
+            'id': img_id,
+            'rotation': qvec,
+            'translation': tvec
+        })
+        # filter only useful point2d data
+        for i in range(len(xys)):
+            if point3d_ids[i] != -1:
+                x,y = xys[i]
+                point2d.append({
+                    'image_id': img_id,
+                    'camera_id': camera_id,
+                    'point3d_id': point3d_ids[i],
+                    'position': xys[i]
+                })
         
 
-
+    return (point2d, instrinsic, extrinsic, point3d)
 
 
 def database_reader():
--- deeparch/writer.py
+++ deeparch/writer.py
@@ -1,2 +1,96 @@
-def write_file(poin2d,intrinsic,extrinsic,point3d):
-    pass
+import numpy as np
+from scipy.spatial.transform import Rotation
+
+def instrinsic_builder(intrisics):
+    model = intrisics['model']
+    params = intrisics['params']
+    params.astype(np.float32)
+    if model == 'SIMPLE_PINHOLE':
+        return '{:f} {:f} 1 {:f} 0\n'.format(
+            params[1],
+            params[2],
+            params[0],
+        )
+    elif model == 'PINHOLE':
+        return "{:f} {:f} 2 {:f} {:f} 0\n".format(
+            params[2],
+            params[3],
+            params[0],
+            params[1],
+        )
+    elif model == 'SIMPLE_RADIAL':
+        return '{:f} {:f} 1 {:f} 1 {:f}\n'.format(
+            params[1],
+            params[2],
+            params[0],
+            params[3] 
+        )
+    elif model == 'RADIAL':
+        return '{:f} {:f} 1 {:f} 2 {:f} {:f}\n' [
+            params[1],
+            params[2],
+            params[0],
+            params[3],
+            params[4]
+        ]
+    else:
+        raise RuntimeError(
+            'Camera number {} using {} which is currently not support'
+                .format(intrisics['id'], model)
+        )
+
+def write_file(output_path, data):
+    api_version = 0.01
+    point2ds, intrinsics, extrinsics, point3ds = data
+    with open(output_path,'w') as f:
+        f.write('{:f}\n'.format(api_version)) #version at head
+        #print file number
+        f.write('{:d} {:d} {:d} {:d}\n'.format(
+            len(point2ds),
+            len(intrinsics),
+            len(extrinsics),
+            len(point3ds)
+        ))
+        point3d_ids = {}
+        point3d_count = 0
+
+        #build lookup for point2d
+        for point3d in point3ds:
+            point3d_ids[point3d['id']] = point3d_count
+            point3d_count = point3d_count + 1
+
+        for point2d in point2ds:
+            x,y = point2d['position']
+            f.write('{:d} {:d} {:d} {:f} {:f}\n'.format(
+               int(point2d['camera_id'] - 1), #index start from 0
+               int(point2d['image_id'] - 1),
+               int(point3d_ids[point2d['point3d_id']]),
+               float(x),
+               float(y)
+            ))
+            
+        for intrinsic in intrinsics:
+            f.write(instrinsic_builder(intrinsic))
+
+        for extrinsic in extrinsics:
+            tvec = extrinsic['translation']
+            qvec = extrinsic['rotation']
+            rotation = Rotation.from_quat([qvec[1],qvec[2],qvec[3],qvec[0]])
+            rotvec = rotation.as_rotvec()
+            f.write('{:f} {:f} {:f} 3 {:f} {:f} {:f}\n'.format(
+                tvec[0],
+                tvec[1],
+                tvec[2],
+                rotvec[0],
+                rotvec[1],
+                rotvec[2]
+            ))
+        
+        for point3d in point3ds:
+            x, y, z = point3d['position']
+            r, g, b = point3d['color']
+            f.write('{:f} {:f} {:f} {:d} {:d} {:d}\n'.format(
+                float(x), float(y), float(z), int(r), int(g), int(b)
+            ))
+
+        
